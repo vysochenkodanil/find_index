3a1f571159e36f6f41d6753a4ed8f26a0b9a2d86
Данил Высоченко <vysochenkodanil@gmail.com>
2024-10-26 13:27:54 +0300

    find_index

---
+++ find_index.py
@@ -0,0 +1,23 @@
+def main(a, b):
+    
+    result = 0
+    if b in a:
+        result = a.index(b)
+        return result
+    else:
+        for i in a:
+            if b < i:
+                result = a.index(i)
+                break
+            else:
+                result = len(a)
+               
+    return result
+        
+if __name__ == '__main__':
+    a = list(map(int, input().split()))
+    b = int(input())
+    
+    
+    result = main(a, b)
+    print(result)
